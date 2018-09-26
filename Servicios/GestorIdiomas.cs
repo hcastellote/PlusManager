928924d170c943bc715916722c65ce73fdf2c8eb
hcastellote <43122213+hcastellote@users.noreply.github.com>
2018-09-26 16:48:33 -0300

    Add files via upload

---
+++ Servicios/GestorIdiomas.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Idioma;
+using DAL;
+
+namespace Servicios
+{
+    public class GestorIdiomas
+    {
+        private Idioma.Idioma _unIdioma;
+        private DAL_Idioma _unIdiomaDAL;
+
+        public GestorIdiomas() {
+            _unIdioma = new Idioma.Idioma();
+            _unIdiomaDAL = new DAL_Idioma();
+        }
+
+        public GestorIdiomas(string descripcion, Dictionary<string,string> traducciones) {
+            _unIdioma = new Idioma.Idioma();
+            _unIdiomaDAL = new DAL_Idioma();
+        }
+
+        public Idioma.Idioma seleccionarIdioma(int IdiomaID) {
+            return _unIdioma = _unIdiomaDAL.cambiarIdioma(IdiomaID);
+        }
+
+        public List<Idioma.Idioma> listarIdiomas()
+        {
+            return _unIdiomaDAL.listarIdiomas();
+        }
+
+        public List<string> listarEtiquetas()
+        {
+            return _unIdiomaDAL.listarEtiquetas();
+        }
+
+        public Dictionary<string,string> listarTraducciones(string codigoIdioma)
+        {
+            return _unIdiomaDAL.listarTraducciones(codigoIdioma);
+        }
+
+        public string crearIdioma(Idioma.Idioma _unIdioma) {
+            return _unIdiomaDAL.crearIdioma(_unIdioma);
+        }
+
+        public string ActualizarIdioma(Idioma.Idioma _unIdioma)
+        {
+            return _unIdiomaDAL.ActualizarIdioma(_unIdioma);
+        }
+    }
+
+}
---
+++ Servicios/GestorPermisos.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using BE;
+using System.Windows.Forms;
+
+namespace Servicios
+{
+    
+    public class GestorPermisos
+    {
+        private DAL_Permisos _miListaPermisos = new DAL_Permisos();
+        private DAL_Permisos _miListaPermisosUsuario = new DAL_Permisos();
+        private List<string> listaPermisos;
+        public Componente ObternerPermisos() {
+            return _miListaPermisos.GetListPermisos();
+        }
+
+        public Componente GetListPermisosUsuario(string usuarioCodigo) {
+            return _miListaPermisosUsuario.GetListPermisosUsuario(usuarioCodigo);
+        }
+
+        public string ActualizarPermisosUsuario(TreeNodeCollection arbolPermisos, string miUsuario)
+        {
+            listaPermisos = new List<string>();
+            llenarListaPermisosString(arbolPermisos);
+            return _miListaPermisosUsuario.ActualizarPermisosUsuario(listaPermisos,miUsuario);
+        }
+
+        private void llenarListaPermisosString(TreeNodeCollection arbolPermisos)
+        {
+            string codigoPermiso = "";
+            foreach (TreeNode miPermiso in arbolPermisos)
+            {
+                codigoPermiso = miPermiso.Text.Substring(0, miPermiso.Text.IndexOf("-") - 1);
+                listaPermisos.Add(codigoPermiso);
+                if (miPermiso.Nodes.Count > 0)
+                    llenarListaPermisosHijosString(miPermiso);
+
+            }
+        }
+
+        private void llenarListaPermisosHijosString(TreeNode arbolPermisos)
+        {
+            string codigoPermiso = "";
+            foreach (TreeNode miPermiso in arbolPermisos.Nodes)
+            {
+                codigoPermiso = miPermiso.Text.Substring(0, miPermiso.Text.IndexOf("-") - 1);
+                listaPermisos.Add(codigoPermiso);
+                if (miPermiso.Nodes.Count > 0)
+                    llenarListaPermisosHijosString(miPermiso);
+            }
+        }
+
+
+    }
+}
